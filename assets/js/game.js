1bb27ec2c32117363cdf96a3fa72d2ca3db28f4a
nhodges <hodges27@outlook.com>
2021-09-08 19:34:57 -0400

    multiple fight rounds

--- assets/js/game.js
+++ assets/js/game.js
@@ -14,8 +14,8 @@ var enemyHealth = 50;
 var enemyAttack = 12;
 
 var fight = function(enemyName) {
-    // Alert players that they are starting the round
-    window.alert("Welcome to Robot Glaidators!");
+    // repeat and execute as long as the enemy-robot is alive
+    while(enemyHealth > 0) {
 
     var promptFight = window.prompt("Would you like to FIGHT or SkIP this battle? Enter 'FIGHT' or 'SKIP' to choose.");
     // if player chooses to fight, then fight
@@ -63,8 +63,11 @@ var fight = function(enemyName) {
       } else {
         window.alert("You need to choose a valid option. Try again!");
     }
+    }   
 };
 
 for(var i = 0; i < enemyNames.length; i++) {
-    fight(enemyNames[i]);
+    var pickedEnemyName = enemyNames[i];
+    enemyHealth = 50;
+    fight(pickedEnemyName);
 }
