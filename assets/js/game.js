b0ed6fe24f61fdf0159340e92448b80764e3287d
nhodges <hodges27@outlook.com>
2022-05-22 13:57:35 -0400

    game info

--- assets/js/game.js
+++ assets/js/game.js
@@ -235,4 +235,54 @@ var getPlayerName = function() {
   
 /* END GAME FUNCTIONS */
 
+/* GAME INFORMATION / VARIABLES */
+
+var playerInfo = {
+    name: getPlayerName(),
+    health: 100,
+    attack: 10,
+    money: 10,
+    reset: function() {
+      this.health = 100;
+      this.money = 10;
+      this.attack = 10;
+    },
+    refillHealth: function() {
+      if (this.money >= 7) {
+        window.alert("Refilling player's health by 20 for 7 dollars.");
+        this.health += 20;
+        this.money -= 7;
+      } else {
+        window.alert("You don't have enough money!");
+      }
+    },
+    upgradeAttack: function() {
+      if (this.money >= 7) {
+        window.alert("Upgrading player's attack by 6 for 7 dollars.");
+        this.attack += 6;
+        this.money -= 7;
+      } else {
+        window.alert("You don't have enough money!");
+      }
+    }
+};
+  
+var enemyInfo = [
+    {
+      name: "Roborto",
+      attack: randomNumber(10, 14)
+    },
+    {
+      name: "Amy Android",
+      attack: randomNumber(10, 14)
+    },
+    {
+      name: "Robo Trumble",
+      attack: randomNumber(10, 14)
+    }
+];
+  
+/* END GAME INFORMATION / VARIABLES */
+
+/* RUN GAME */
 startGame();
